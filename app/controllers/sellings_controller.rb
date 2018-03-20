ca68638a2ee95431bf984f7c104eb0ad99c6cb9f
Lucas Santos <lucas.santos.silva2011@gmail.com>
2018-03-20 13:40:06 -0300

    feature: added insert action

--- app/controllers/sellings_controller.rb
+++ app/controllers/sellings_controller.rb
@@ -5,9 +5,16 @@ class SellingsController < ApplicationController
   end
 
   def new
+    @selling = Selling.new
   end
 
   def create
+    data = sellings_params[:file].read.split(/\r?\n|\r/).drop(1)
+    sellings = get_sellings_from_data(data)
+
+    fast_insert(sellings)
+
+    redirect_to sellings_path
   end
 
   def show
@@ -16,5 +23,22 @@ class SellingsController < ApplicationController
   private
     def sellings_params
       params.require(:selling).permit(:file)
+    end
+
+    def get_sellings_from_data(data)
+      values = []
+
+      data.drop(0).each do |selling|
+        s = selling.split(/\t/)
+        values << "(\"#{s[0]}\", \"#{s[1]}\", #{s[2].to_f}, #{s[3].to_f}, \"#{s[4]}\", \"#{s[5]}\")"
+      end
+
+      values
+    end
+
+    def fast_insert(values)
+      ActiveRecord::Base.connection.execute(
+        "INSERT INTO sellings (buyer, description, unit_price, amount, address, provider) VALUES #{values.join(',')}"
+      )
     end
 end
