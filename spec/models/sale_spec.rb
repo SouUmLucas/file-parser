98062aba869611312ab3397ea460b41f637f5d51
Lucas Santos <lucas.santos.silva2011@gmail.com>
2018-03-20 14:44:42 -0300

    refactor: tests

--- spec/models/sale_spec.rb
+++ spec/models/sale_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Sale, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to respond_to :buyer }
+  it { is_expected.to respond_to :description }
+  it { is_expected.to respond_to :unit_price }
+  it { is_expected.to respond_to :amount }
+  it { is_expected.to respond_to :address }
+  it { is_expected.to respond_to :provider }
 end
