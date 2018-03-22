0e9b7ff22ca619ac8caa134cf0ef265a8fb0e3d0
Lucas Santos <lucas.santos.silva2011@gmail.com>
2018-03-22 00:33:14 -0300

    test: added model test

--- spec/models/sale_spec.rb
+++ spec/models/sale_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Sale, type: :model do
+  it { should belong_to :import_file }
+  
   it { is_expected.to respond_to :buyer }
   it { is_expected.to respond_to :description }
   it { is_expected.to respond_to :unit_price }
