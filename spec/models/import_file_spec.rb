07b7a601237fe00daaf20b381812a12db8c25d96
Lucas Santos <lucas.santos.silva2011@gmail.com>
2018-03-22 01:01:14 -0300

    test: added model test

--- spec/models/import_file_spec.rb
+++ spec/models/import_file_spec.rb
@@ -4,4 +4,17 @@ RSpec.describe ImportFile, type: :model do
   it { should have_many :sales }
   
   it { is_expected.to respond_to :id }
+
+  context 'test gross revenue' do
+    before do
+      test_file = File.new("#{Rails.root}/spec/support/dados_test.txt", 'r')
+      @file = ImportFile.new
+      @file.file = test_file
+      @file.save
+    end
+
+    it 'should return gross revenue 95.0' do
+      expect(@file.gross_revenue.to_f).to eq(95.0)
+    end
+  end
 end
